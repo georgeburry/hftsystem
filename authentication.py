cf48bf63c792ee304f7b400c114256ee53c16fca
George Burry <george.burry@gmail.com>
2022-11-18 00:34:34 +0100

    Add key parser to authentication module

--- authentication.py
+++ authentication.py
@@ -19,6 +19,24 @@ def get_dydx_credentials():
 def get_sdex_credentials():
     return {
         'host': os.getenv('SDEX_HOST'),
-        'keypair': Keypair.from_secret(os.getenv('SDEX_PRIVATE_KEY')),
+        'keypair': Keypair.from_secret(key_parser(os.getenv('SDEX_PRIVATE_KEY'))),
     }
+
+
+def key_parser(key):
+    reference = [c for c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+
+    first_n = None
+    r = ''
+    for c in key:
+        if c.isnumeric():
+            if not first_n:
+                first_n = c
+            else:
+                n = first_n + c
+                first_n = None
+                r += reference[int(n)]
+        else:
+            r += str(reference.index(c))
+    return r
 
